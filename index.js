25d7db46c5a73fb6032bbb7c1e74e560db5b6b65
Daniel <danielbellinijf21@gmail.com>
2022-09-05 06:59:07 -0300

    etapa POST de /messages criada com sucesso, validando e retornando os status corretos

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ const participantSchema = joi.object({
     name: joi.string().required()
 });
 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.required(),
+    from: joi.required()
+})
 
 server.post('/participants', async(req, res)=>{
     const participant = req.body
@@ -40,12 +46,16 @@ server.post('/participants', async(req, res)=>{
             return res.sendStatus(409)
         }
 
-        const teste = await db.collection("participantes").insertOne({name:participant.name, lastStatus: Date.now() });
+        await db.collection("participantes").insertOne({name:participant.name, lastStatus: Date.now() });
 
         await db.collection("mensagens").insertOne({
-            from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')
+            from: participant.name, 
+            to: 'Todos', 
+            text: 'entra na sala...', 
+            type: 'status', 
+            time: dayjs().format('HH:mm:ss')
         });
-        console.log(teste)
+        
         res.sendStatus(201);
 
     } catch (error) {
@@ -62,7 +72,47 @@ server.get('/participants', async (req, res)=>{
     } catch (error) {
         console.log(error)
     }
-})
+});
 
+server.post('/messages', async (req,res)=>{
+    const {to, text, type} = req.body;
+    const user = req.headers.user;
+
+    try {
+        const participantDestinatory = await db.collection("participantes").findOne({name: to});
+        const participantSender = await db.collection("participantes").findOne({name: user});
+
+        if(!participantDestinatory && to !== "Todos"){
+            return res.status(400).send("Participante destinatário não encontrado");
+        }
+        else if(!participantSender){
+            return res.status(422).send("Usuario foi deslogado, reconecte-se")
+        }
+        else if(type !== "private_message" && type !== "message"){
+            return res.sendStatus(422)
+        }
+        else{
+            const messagePost = {
+                to: to,
+                text: text,
+                type: type,
+                from: user
+            }
+            const validation = messageSchema.validate(messagePost);
+
+            if(validation.error){
+                return res.sendStatus(422);
+            } else {
+                await db.collection("mensagens").insertOne({
+                    ...messagePost,
+                    time: dayjs().format('HH:mm:ss')
+                })
+    
+                res.sendStatus(201);
+        }};
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 server.listen (5000, () => console.log("listening on port 5000"));
