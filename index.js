29550aafcb863e13c0cfcf9e77c98080ca7e23b1
Daniel <danielbellinijf21@gmail.com>
2022-09-05 02:14:44 -0300

    post /participants consegue inserir e verificar novos participants

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 import express from "express";
 import cors from "cors";
-import {MongoClient, MongoClient, ObjectId} from "mongodb";
-import joi from joi;
+import { MongoClient } from "mongodb";
+import joi from "joi";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config()
+
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+//aparentemente tive problemas com o documento .env, antes de finalizar a entrega tentarei concertar...
 
 let db;
 
 mongoClient.connect().then(()=>{
     db =mongoClient.db("batepapoUOL")
+})
+
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
+
+
+server.post('/participants', async(req, res)=>{
+    const participant = req.body
+
+    const validation = participantSchema.validate(participant);
+
+    if (validation.error){
+        return res.sendStatus(422)
+    }
+
+    try {
+        const participantFound = await db.collection('participants').findOne({name: participant.name})
+        if (participantFound){
+            return res.sendStatus(409)
+        }
+
+        await db.collection('participants').insertOne({name:participant.name})
+
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
