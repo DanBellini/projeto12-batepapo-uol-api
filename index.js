8124f46b173c069333971480cbbaeb2946e13c43
Daniel <danielbellinijf21@gmail.com>
2022-09-05 04:25:34 -0300

    metodo GET de participants implementado

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient("mongodb://localhost:27017/");
 //aparentemente tive problemas com o documento .env, antes de finalizar a entrega tentarei concertar...
 
 let db;
 
 mongoClient.connect().then(()=>{
-    db =mongoClient.db("batepapoUOL")
-})
+    db = mongoClient.db("batepapo-uol")
+});
 
 const participantSchema = joi.object({
     name: joi.string().required()
@@ -35,23 +35,34 @@ server.post('/participants', async(req, res)=>{
     }
 
     try {
-        const participantFound = await db.collection('participants').findOne({name: participant.name})
+        const participantFound = await db.collection("participantes").findOne({name: participant.name})
         if (participantFound){
             return res.sendStatus(409)
         }
 
-        await db.collection('participants').insertOne({name:participant.name, lastStatus: Date.now() });
+        const teste = await db.collection("participantes").insertOne({name:participant.name, lastStatus: Date.now() });
 
-        await db.collection('messages').insertOne({
+        await db.collection("mensagens").insertOne({
             from: participant.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')
         });
-
+        console.log(teste)
         res.sendStatus(201);
 
     } catch (error) {
         console.log(error)
     }
 });
+
+server.get('/participants', async (req, res)=>{
+    try {
+        const participants = await db.collection("participantes").find().toArray();
+
+        res.send(participants);
+
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
 server.listen (5000, () => console.log("listening on port 5000"));
